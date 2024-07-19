8e1dfafd5fd10aaef693515f0fc3fc3740c39459
Germey <cqc@cuiqingcai.com>
2024-07-20 00:38:43 +0800

    fix

--- api/core/model_runtime/model_providers/acedatacloud/_common.py
+++ api/core/model_runtime/model_providers/acedatacloud/_common.py
@@ -1,6 +1,8 @@
 from collections.abc import Mapping
-
 from httpx import Timeout
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class _CommonAceDataCloud:
@@ -18,6 +20,7 @@ class _CommonAceDataCloud:
             "max_retries": 1,
         }
 
-        credentials_kwargs["base_url"] = 'https://api.acedatacloud.com/openai/'
-
+        credentials_kwargs["base_url"] = 'https://api.acedata.cloud/openai/'
+        # credentials_kwargs["base_model_name"] = 'gpt-3.5-turbo'
+        logger.debug(f"credentials_kwargs: {credentials}")
         return credentials_kwargs
